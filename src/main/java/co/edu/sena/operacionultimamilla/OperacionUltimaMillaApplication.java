c341a7f5e63d426da37ee9f732b9590645a97321
Jonas <stivencamacho2006@gmail.com>
2026-08-27 08:25:31 -0500

    Crear proyecto inicial de Spring Boot

---
+++ src/main/java/co/edu/sena/operacionultimamilla/OperacionUltimaMillaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.operacionultimamilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacionUltimaMillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacionUltimaMillaApplication.class, args);
+	}
+
+}
